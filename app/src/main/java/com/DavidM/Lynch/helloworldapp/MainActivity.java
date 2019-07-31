3b75112b25d27ec0331b9cdc572610918d9c7fcd
David Lynch <enkidu_3@yahoo.com>
2019-07-31 13:42:45 -0400

    Initial commit

---
+++ app/src/main/java/com/DavidM/Lynch/helloworldapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.DavidM.Lynch.helloworldapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
